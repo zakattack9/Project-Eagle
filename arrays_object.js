02c113ac027c5cb6ea0bad4611e06587a47536b9
zakattack9 <zakattack26111@gmail.com>
2016-12-05 08:39:38 -1000

    Caccaaaaw!

---
+++ arrays_object.js
@@ -0,0 +1,25 @@
+/*
+
+  Zak Sakata
+  Arrays and Objects
+  Dec. 5, 2016
+
+*/
+
+//Variables
+//Arrays
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var names = ["Senior Tan", "Zakattack", "YoloMcFoloHolo", "Ohai", "Jeff"];
+var colors = ["booger green", "dark black", "doodoo brown"];
+var adjective = ["smart", "colorful", "nasty", "stink", "dumb"];
+var slang = ["scrap", "beef", "da kine", "la dat", "broke da mout", "shoots braddah"];
+
+//Objects
+
+var donut = {
+  toppings:["rainbow sprinkles", "blue chocolate", "thick air"],
+  fillings:["jelly", "chocolate"],
+  shape:["octagon", "dodecagon", "parabola", "unit circle", "square"],
+  count: 9
+}
